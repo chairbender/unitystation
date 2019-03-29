8413bedd97c3a9210094f815e74b48fa5fec8521
chairbender <kwhipke1@gmail.com>
2019-03-29 12:14:11 -0700

    #1488 can open and close a blank dev spawner menu

---
+++ UnityProject/Assets/Scripts/UI/DevTabButtons.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Behavior for the various buttons on the Dev tab
+/// </summary>
+public class DevTabButtons : MonoBehaviour
+{
+	public void BtnSpawnItem()
+	{
+		//TODO: Pop up item spawner window
+		GUI_DevSpawner.Instance.Open();
+	}
+}
---
+++ UnityProject/Assets/Scripts/UI/PopUps/GUI_DevSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Main logic for the dev spawner menu
+/// </summary>
+public class GUI_DevSpawner : MonoBehaviour
+{
+	public static GUI_DevSpawner Instance;
+
+	void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		//TODO: Not sure if this is needed, its used in GUI_IngameMenu
+		/*
+		else
+		{
+			Destroy(gameObject);
+		}
+		*/
+	}
+
+	public void Open()
+	{
+		SoundManager.Play("Click01");
+		Logger.Log("Opening dev spawner menu", Category.UI);
+		transform.GetChild(0).gameObject.SetActive(true);
+		transform.SetAsLastSibling();
+	}
+
+	public void Close()
+	{
+		SoundManager.Play("Click01");
+		Logger.Log("Closing dev spawner menu", Category.UI);
+		transform.GetChild(0).gameObject.SetActive(false);
+	}
+}
